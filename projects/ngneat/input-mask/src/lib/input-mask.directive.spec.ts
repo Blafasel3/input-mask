6503ca26ceeb06a54a4ed156159543ee35ce7b42
Dharmen Shah <dharmen@solvative.com>
2021-05-08 14:40:49 +0530

    test: add specs

--- projects/ngneat/input-mask/src/lib/input-mask.directive.spec.ts
+++ projects/ngneat/input-mask/src/lib/input-mask.directive.spec.ts
@@ -1,16 +1,57 @@
-import { InputMaskDirective } from '@ngneat/input-mask';
-import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  createComponentFactory,
+  Spectator,
+  typeInElement,
+} from '@ngneat/spectator';
+import { createMask } from './input-mask.directive';
+import { InputMaskModule } from './input-mask.module';
+
+@Component({
+  template: `
+    <input class="input" [inputMask]="dateMask" [formControl]="dateFC" />
+  `,
+})
+class TestComponent {
+  dateMask = createMask({
+    alias: 'datetime',
+    inputFormat: 'dd/mm/yyyy',
+    parser: (value: string) => {
+      const values = value.split('/');
+      const year = +values[2];
+      const month = +values[1] - 1;
+      const date = +values[0];
+      return new Date(year, month, date);
+    },
+  });
+  dateFC = new FormControl('');
+}
 
 describe('HighlightDirective', () => {
-  let spectator: SpectatorDirective<InputMaskDirective>;
-  const createDirective = createDirectiveFactory(InputMaskDirective);
+  let spectator: Spectator<TestComponent>;
+  const createComponent = createComponentFactory({
+    component: TestComponent,
+    imports: [ReactiveFormsModule, InputMaskModule],
+  });
 
   beforeEach(() => {
-    spectator = createDirective(`<input inputMask>`);
+    spectator = createComponent();
   });
 
   it('should get the instance', () => {
-    const instance = spectator.directive;
+    const instance = spectator.component;
     expect(instance).toBeDefined();
+  });
+
+  it('should update the UI value as per mask', () => {
+    const input = spectator.query('.input') as HTMLInputElement;
+    spectator.typeInElement('28021992', '.input');
+    expect(input.value).toEqual('28/02/1992');
+  });
+
+  it('should update the control value as per mask parser', () => {
+    spectator.typeInElement('28021992', '.input');
+    expect(spectator.component.dateFC.value).toEqual(new Date(1992, 1, 28));
   });
 });
